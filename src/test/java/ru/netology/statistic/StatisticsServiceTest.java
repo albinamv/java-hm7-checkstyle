07573c8e63ef7b32378fc0915e880d70ba1a6a11
Albina MV <alyamarvlad@gmail.com>
2022-03-19 21:20:57 +1000

    Test added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,4 +1,33 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StatisticsServiceTest {
-  
+
+    @Test
+    void findMaxIfItIsFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIfItIsNotFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 9, 4, 5, 3, 8, 13, 11, 11, 8};
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
 }
